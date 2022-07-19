3d84e4d9699cf63ea2130194d517c8a56e269660
Ashley <97846504+AshJungers@users.noreply.github.com>
2022-07-19 11:07:26 -0600

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,41 @@
+let homeScore = document.getElementById("home-score");
+let guestScore = document.getElementById("guest-score");
+let homeCount = 0;
+let guestCount = 0;
+
+function increaseHomeScoreOne() {
+  homeCount += 1;
+  homeScore.textContent = homeCount;
+}
+
+function increaseHomeScoreTwo() {
+  homeCount += 2;
+  homeScore.textContent = homeCount;
+}
+
+function increaseHomeScoreThree() {
+  homeCount += 3;
+  homeScore.textContent = homeCount;
+}
+
+function increaseGuestScoreOne() {
+  guestCount += 1;
+  guestScore.textContent = guestCount;
+}
+
+function increaseGuestScoreTwo() {
+  guestCount += 2;
+  guestScore.textContent = guestCount;
+}
+
+function increaseGuestScoreThree() {
+  guestCount += 3;
+  guestScore.textContent = guestCount;
+}
+
+function newGame() {
+  homeScore.textContent = 0;
+  guestScore.textContent = 0;
+  homeCount = 0;
+  guestCount = 0;
+}
